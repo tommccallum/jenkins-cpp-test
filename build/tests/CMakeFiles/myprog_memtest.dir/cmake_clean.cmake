89067580e2656ae814e5ee160b0525fa5296e046
Tom McCallum <this_is_not_an_email@no_where.com>
2018-12-16 15:41:52 +0000

    added a valgrind memory check

---
+++ build/tests/CMakeFiles/myprog_memtest.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myprog_memtest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/myprog_memtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
