5ee6e69ccf3dc05d56a4800cab49a2dde7a0af06
Tom McCallum <this_is_not_an_email@no_where.com>
2018-12-16 12:51:26 +0000

    Added conan and cmake files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+ #include "Poco/MD5Engine.h"
+ #include "Poco/DigestStream.h"
 
-int main() {
-	std::cout << "Hello Jenkins!" << std::end;
-	return 0;
-}
+ #include <iostream>
+
+
+ int main(int argc, char** argv)
+ {
+     Poco::MD5Engine md5;
+     Poco::DigestOutputStream ds(md5);
+     ds << "abcdefghijklmnopqrstuvwxyz";
+     ds.close();
+     std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+     return 0;
+ }
